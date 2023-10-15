cbc0349e72f22d2f0359c3c5ccab302bd0fce906
Facundo Spira <facundospira@gmail.com>
2023-10-14 22:10:46 -0300

    feat: add ImageCard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,22 @@
 import Layout from "~/components/templates/Layout";
 import { type NextPageWithLayout } from "./_app";
+import ImageCard from "~/components/molecules/ImageCard";
 
 const Home: NextPageWithLayout = () => (
-  <div>
-    <h1 className="text-xl">SyncNema Home</h1>
+  <div className="flex gap-4">
+    <ImageCard
+      name="Oppenheimer"
+      imageUrl="https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_.jpg"
+      id="1"
+      type="movie"
+    />
+
+    <ImageCard
+      name="Barbie"
+      imageUrl="https://www.themoviedb.org/t/p/original/u5kboZR4OMi4QdbOhawCZuzMVWJ.jpg"
+      id="2"
+      type="movie"
+    />
   </div>
 );
 
