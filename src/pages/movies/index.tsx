e5d3742217cbfa0c028d508ea1491316f6874946
Facundo Spira <facundospira@gmail.com>
2023-10-25 20:08:52 -0300

    feat: add tvshow screen with infinite scroller

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -2,11 +2,14 @@ import Layout from "~/components/templates/Layout";
 import { type NextPageWithLayout } from "../_app";
 import { useState } from "react";
 import Filter from "~/components/organisms/Filter";
+import { useDebounce } from "~/hooks/useDebounce";
 
 const Movies: NextPageWithLayout = () => {
   const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
   const [selectedPlatforms, setSelectedPlatforms] = useState<string[]>([]);
   const [search, setSearch] = useState("");
+
+  const debouncedSearch = useDebounce(search, 500);
 
   return (
     <div>
