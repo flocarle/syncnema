f8b5add4e59bfd8498d735cc5fa544e99625485a
gpiria <88985512+gpiria@users.noreply.github.com>
2023-11-01 17:41:05 -0300

    Movies page (#11)

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -3,6 +3,9 @@ import { type NextPageWithLayout } from "../_app";
 import { useState } from "react";
 import Filter from "~/components/organisms/Filter";
 import { useDebounce } from "~/hooks/useDebounce";
+import { generateListings } from "~/utils/listingGenerator";
+import ListingCard from "~/components/molecules/ListingCard";
+import InfiniteScroll from "react-infinite-scroller";
 
 const Movies: NextPageWithLayout = () => {
   const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
@@ -11,6 +14,9 @@ const Movies: NextPageWithLayout = () => {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const debouncedSearch = useDebounce(search, 500);
+
+  //TODO: fetch from API
+  const [movies, setMovies] = useState(generateListings(20, "movie"));
 
   return (
     <div>
@@ -22,6 +28,28 @@ const Movies: NextPageWithLayout = () => {
         setSelectedPlatforms={setSelectedPlatforms}
         setSearch={setSearch}
       />
+      <InfiniteScroll
+        loadMore={async () => {
+          const newMovies = generateListings(20, "movie");
+          await new Promise((resolve) => setTimeout(resolve, 2000));
+          setMovies([...movies, ...newMovies]);
+        }}
+        hasMore={true}
+        loader={<p className="mt-4">Cargando...</p>}
+        className="mt-8 flex flex-col items-center justify-center"
+      >
+        <div className="flex w-full flex-row flex-wrap items-center justify-center gap-4">
+          {movies.map((movie) => (
+            <ListingCard
+              key={movie.id}
+              id={movie.id}
+              name={movie.name}
+              imageUrl={movie.imageUrl}
+              type={movie.listingType}
+            ></ListingCard>
+          ))}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
